8b783f23ad32ab8db6737fa55580025ab04c54f4
crodican <crodican@councilsepa.org>
2025-02-01 08:29:17 -0500

    Updated index.html

--- Philadelphia/script2.js
+++ Philadelphia/script2.js
@@ -1,35 +1,39 @@
 $(document).ready(function () {
     const container = $(".uk-grid-small");
     jsonData.forEach((item) => {
-        const populations = item["Populations Served"].split(";").map((pop) => `<span class="badge rounded-pill text-bg-danger">${pop.trim()}</span>`).join(" ");
-        const counties = item["County"].split(";").map((county) => `<span class="badge rounded-pill text-bg-warning">${county.trim()} County</span>`).join(" ");
+        const populations = item["Populations Served"].split(";").map((pop) => `<span class="uk-label uk-label-danger">${pop.trim()}</span>`).join(" ");
+        const counties = item["County"].split(";").map((county) => `<span class="uk-label uk-label-warning">${county.trim()} County</span>`).join(" ");
         const card = `
             <div class="js-filter-item data-county="${item.County}" data-domain="${item.Domain}">
-                <div class="card shadow">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <span class="badge rounded-pill text-bg-primary">
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-header">
+                        <div class="uk-align-left">
+                            <div class="uk-width-auto">
+                                <span class="uk-label uk-label-primary">
                                 ${item.Domain}
-                                </span>     
-                                <span class="badge rounded-pill text-bg-success">
-                                ${item.Category}
-                                </span>                                
+                                </span>
                             </div>
-                            <div class="col text-end">
-                                ${counties} 
+                            <div class="uk-width-auto">
+                                <span class="uk-label uk-label-success">
+                                ${item.Category}
+                                </span>
+                            </div>
+                        </div>
+                        <div class="uk-align-right">
+                            <div class="uk-width-auto">
+                            ${counties}
                             </div>
                         </div>
                     </div>
-                    <div class="card-body">
-                        <h3 class="card-title">${item["Location Name"]}</h3>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title">${item["Location Name"]}</h3>
                         <h5>${item.Organization}</h5>
                         ${item.Image ? ` <img src = "${item.Image}"
         alt = "Logo"
         style = "max-width:100px; max-height:100px; float:left; margin-right:10px;" > ` : ""}
                         <p><strong>Phone:</strong> ${item.Phone}</p>
-                        <br /><p>${item.Address}<br />
-                        ${item.City}, ${item.State} ${item["Zip Code"]}</p>
+                        <p>${item.Address}</p>
+                        <p>${item.City}, ${item.State} ${item["Zip Code"]}</p>
                         <br />
                         <p><strong>Populations Served:</strong> ${populations}</p>
                         <br />
