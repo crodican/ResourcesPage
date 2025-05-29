3006ff0ec46b99220bffa9b88a9444cc28010503
crodican <crodican@councilsepa.org>
2025-05-28 21:09:06 -0400

    strategic plan

---
+++ strategic_plan_outline.mm
@@ -0,0 +1,328 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Regional Recovery Hubs 1 &amp; 4&#xa;Strategic Plan Outline" LOCALIZED_STYLE_REF="default" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1748456991071" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#ffffff" zoom="1.003">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_grey_straight_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1082331723" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1082331723" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="horizontal" COLOR="#444444" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#3333ff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#3333ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_334768726" COLOR="#443333" BACKGROUND_COLOR="#dddddd" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc0033">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc0033" TRANSPARENCY="255" DESTINATION="ID_334768726"/>
+<font SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#000000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#222222" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#444444" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#676767" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#888888" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#222222" BACKGROUND_COLOR="#aaaaaa" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#222222" BACKGROUND_COLOR="#bbbbbb" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#222222" BACKGROUND_COLOR="#cccccc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#222222" BACKGROUND_COLOR="#dddddd" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Priority 3:&#xa; Stigma Reducation and Education" LOCALIZED_STYLE_REF="default" FOLDED="true" POSITION="bottom_or_right" ID="ID_1262603265" CREATED="1742913723601" MODIFIED="1748457038764" HGAP_QUANTITY="79.11304 pt" VSHIFT_QUANTITY="-156.52174 pt">
+<node LOCALIZED_STYLE_REF="default" ID="ID_603573912" CREATED="1742913888312" MODIFIED="1747783651333"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Why?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Focus Groups and community organizations as well as addiction professionals felt strongly that while efforts are being made in hospitals, police departments, and various other public serving entities there is still vital need for education to change how persons with Substance Use Disorder are addressed and treated." LOCALIZED_STYLE_REF="default" ID="ID_1882497397" CREATED="1742914494703" MODIFIED="1747783651333"/>
+<node TEXT="MAT is still very much misunderstood; continuing to cause barriers for those who embrace this as their recovery pathway." LOCALIZED_STYLE_REF="default" ID="ID_62486531" CREATED="1742914494703" MODIFIED="1747783651334"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_1502543194" CREATED="1742913904403" MODIFIED="1747783651334"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Goal
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Reduce impact of stigma through education and training by 6/30/2025." LOCALIZED_STYLE_REF="default" ID="ID_1818816020" CREATED="1742914511606" MODIFIED="1747783651335"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_1702297558" CREATED="1742913918270" MODIFIED="1747783651335"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Objectives
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Develop Intro to Recovery Support Services Training and deliver a minimum of 7 orientations to various providers and students across the region." LOCALIZED_STYLE_REF="default" ID="ID_460931870" CREATED="1742914544607" MODIFIED="1747783651335"/>
+<node TEXT="Utilize “Our Stories Have Power” training and deliver 3 trainings within the region." LOCALIZED_STYLE_REF="default" ID="ID_1315603097" CREATED="1742914544607" MODIFIED="1747783651337"/>
+</node>
+</node>
+<node TEXT="Priority 4:&#xa; Recovery Community Centers" LOCALIZED_STYLE_REF="default" FOLDED="true" POSITION="bottom_or_right" ID="ID_1781481343" CREATED="1742913732765" MODIFIED="1748457041413" HGAP_QUANTITY="42.8 pt" VSHIFT_QUANTITY="0.62609 pt">
+<node LOCALIZED_STYLE_REF="default" ID="ID_858331961" CREATED="1742913888855" MODIFIED="1747783651338"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Why?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="RCCs are limited in number across the Region and struggle with sustainability. These centers generally only serve the local community due to barriers such as transportation and travel time. This indicates a need for possible satellite sites and/or mobile engagement." LOCALIZED_STYLE_REF="default" ID="ID_905512140" CREATED="1742914573937" MODIFIED="1747783651339"/>
+<node TEXT="People in recovery need a safe space to build a support network and have access to resources" LOCALIZED_STYLE_REF="default" ID="ID_501043248" CREATED="1742914573937" MODIFIED="1747783651340"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_240234710" CREATED="1742913904929" MODIFIED="1747783651340"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Goal
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Support the implementation and/or growth of Recovery Community Centers by 6/30/2025." LOCALIZED_STYLE_REF="default" ID="ID_534061397" CREATED="1742914581456" MODIFIED="1747783651340"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_360209452" CREATED="1742913918702" MODIFIED="1747783651341"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Objectives
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Assess the RCC’s need for individual and group recovery support services." LOCALIZED_STYLE_REF="default" ID="ID_1722223904" CREATED="1742914611948" MODIFIED="1747783651341"/>
+<node TEXT="Support the identified areas within the region that are currently without RCC’s." LOCALIZED_STYLE_REF="default" ID="ID_448642606" CREATED="1742914611948" MODIFIED="1747783651341"/>
+</node>
+</node>
+<node TEXT="Priority 5:&#xa; Housing and Transportation" LOCALIZED_STYLE_REF="default" FOLDED="true" POSITION="bottom_or_right" ID="ID_1352004159" CREATED="1742913734633" MODIFIED="1748457044065" HGAP_QUANTITY="72.85218 pt" VSHIFT_QUANTITY="131.47826 pt">
+<node LOCALIZED_STYLE_REF="default" ID="ID_651150758" CREATED="1742913889453" MODIFIED="1747783651342"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Why?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Through meeting with multiple organizations and having focus groups across the Region, there is an excessive amount of people who are unhoused which makes it more difficult for them to access recovery resources and support." LOCALIZED_STYLE_REF="default" ID="ID_1595077710" CREATED="1742914632154" MODIFIED="1747783651343"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_824004579" CREATED="1742913905457" MODIFIED="1747783651343"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Goal
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Enhance housing and transportation resources across the region by 6/30/2025." LOCALIZED_STYLE_REF="default" ID="ID_1899600924" CREATED="1742914645127" MODIFIED="1747783651343"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_1102958251" CREATED="1742913919152" MODIFIED="1747783651344"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Objectives
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Provide support to existing Recovery Houses." LOCALIZED_STYLE_REF="default" ID="ID_1203196872" CREATED="1742914681808" MODIFIED="1747783651344"/>
+<node TEXT="Identify strategies and resources to overcome the transportation barriers within the region." LOCALIZED_STYLE_REF="default" ID="ID_1062991236" CREATED="1742914681808" MODIFIED="1747783651344"/>
+</node>
+</node>
+<node TEXT="Proirity 1:&#xa;1. Expansion of the CRS Workforce" LOCALIZED_STYLE_REF="default" POSITION="top_or_left" ID="ID_1272354731" CREATED="1742913711182" MODIFIED="1748457064091" HGAP_QUANTITY="82.24348 pt" VSHIFT_QUANTITY="-134.6087 pt">
+<font SIZE="24"/>
+<node LOCALIZED_STYLE_REF="default" ID="ID_658033173" CREATED="1742913884401" MODIFIED="1747783651318"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Why?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="CRS are really in demand right now." LOCALIZED_STYLE_REF="default" ID="ID_787268534" CREATED="1742914126543" MODIFIED="1747783651320">
+<node TEXT="There are a lot of open positions throughout the region." LOCALIZED_STYLE_REF="default" POSITION="bottom_or_right" ID="ID_495557231" CREATED="1742914126543" MODIFIED="1747783651321"/>
+</node>
+<node TEXT="Right now there are peers who are currently working in positions without the proper training." LOCALIZED_STYLE_REF="default" ID="ID_1140084962" CREATED="1742914126559" MODIFIED="1747783651322"/>
+<node TEXT="There are Reported waitlists for Recovery Specialist classes across region." LOCALIZED_STYLE_REF="default" ID="ID_1303580608" CREATED="1742914126562" MODIFIED="1747783651324"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_415270800" CREATED="1742913902993" MODIFIED="1747783651324"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Goal
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Increase the number of certified individuals by 6/30/2025." LOCALIZED_STYLE_REF="default" ID="ID_1925768082" CREATED="1742914153391" MODIFIED="1747783651324"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_1367923998" CREATED="1742913917254" MODIFIED="1747783651325"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Objectives
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="To ensure that at least 100 candidates participate in the Recovery Specialist Training across the Region by 6/30/25." LOCALIZED_STYLE_REF="default" ID="ID_409901274" CREATED="1742914166813" MODIFIED="1747783651325"/>
+<node TEXT=" Guarantee a minimum of 10 Recovery Specialist Trainings are delivered across the Region by 6/30/2025." LOCALIZED_STYLE_REF="default" ID="ID_331328261" CREATED="1742914166813" MODIFIED="1747783651326"/>
+<node TEXT="Identify trained individuals and assist them through the certification process." LOCALIZED_STYLE_REF="default" ID="ID_1235003214" CREATED="1742914166813" MODIFIED="1747783651327"/>
+<node TEXT="Develop and offer a series of additional trainings that meet the requirements for CRSS Credential." LOCALIZED_STYLE_REF="default" ID="ID_759386967" CREATED="1742914166813" MODIFIED="1747783651327"/>
+</node>
+</node>
+<node TEXT="Priority 2:&#xa;Expansion of CFRS / FAMILY Resources" LOCALIZED_STYLE_REF="default" FOLDED="true" POSITION="top_or_left" ID="ID_1437159472" CREATED="1742913720836" MODIFIED="1748457048241" HGAP_QUANTITY="42.8 pt" VSHIFT_QUANTITY="-2.50435 pt">
+<node LOCALIZED_STYLE_REF="default" ID="ID_96383078" CREATED="1742913887558" MODIFIED="1747783651329"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Why?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Focus Groups, Surveys, and Needs Assessment results showed a lack of family support, resources, and trained individuals across the region." LOCALIZED_STYLE_REF="default" ID="ID_226309124" CREATED="1742914210161" MODIFIED="1747783651329"/>
+<node TEXT="Families expressed a need for more educational opportunities to learn about their loved one’s addiction." LOCALIZED_STYLE_REF="default" ID="ID_1331478560" CREATED="1742914210161" MODIFIED="1747783651330"/>
+<node TEXT="Provider surveys indicated very few facilities offer ongoing family support programming." LOCALIZED_STYLE_REF="default" ID="ID_374692877" CREATED="1742914210193" MODIFIED="1747783651331"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_1865559576" CREATED="1742913903843" MODIFIED="1747783651331"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Goal
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Increase the number of family resources by 6/30/2025." LOCALIZED_STYLE_REF="default" ID="ID_689716234" CREATED="1742914385121" MODIFIED="1747783651332"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_1420459095" CREATED="1742913917781" MODIFIED="1747783651332"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0pt; margin-bottom: 8pt; font-family: Calibri; font-size: 12.0pt; color: black">
+      Objectives
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Increase the number of family groups and individual support across the Region by 6/30/2025." LOCALIZED_STYLE_REF="default" ID="ID_1810386568" CREATED="1742914411544" MODIFIED="1747783651332"/>
+<node TEXT="Increase the number of Certified Family Recovery Specialists by 6/30/2025." LOCALIZED_STYLE_REF="default" ID="ID_975620176" CREATED="1742914411544" MODIFIED="1747783651333"/>
+</node>
+</node>
+</node>
+</map>
